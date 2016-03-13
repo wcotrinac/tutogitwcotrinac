9feceeb6e3c19dab0ac2c80d7c665ad371d79d26
wilmer <EDUARDO@EDUARDO-PC1>
2016-03-12 23:52:25 -0500

    comit test ok

--- appgit/src/fuentes/Persona.java
+++ appgit/src/fuentes/Persona.java
@@ -6,6 +6,8 @@ public class Persona {
 	//cambio1.2
 	
 	private String nombre;
+	private String apellido;
+	
 
 	public String getNombre() {
 		return nombre;
@@ -13,6 +15,14 @@ public class Persona {
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
 	}
 	
 	
