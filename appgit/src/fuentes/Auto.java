6efeeffc1b0d4961af4c0f50894f9d8e3495b5e8
wilmer <EDUARDO@EDUARDO-PC1>
2016-03-12 21:34:55 -0500

    comit auto

---
+++ appgit/src/fuentes/Auto.java
@@ -0,0 +1,19 @@
+package fuentes;
+
+public class Persona {
+	
+	//cambio1
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+}
