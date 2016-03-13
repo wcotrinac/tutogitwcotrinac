157c66a5d2cb08ed059fd655462925804aeaf820
wilmer <EDUARDO@EDUARDO-PC1>
2016-03-12 21:39:40 -0500

    comit other file

---
+++ appgit/src/fuentes/OtherFile.java
@@ -0,0 +1,17 @@
+package fuentes;
+
+public class OtherFile {
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+}
