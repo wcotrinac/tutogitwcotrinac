2ce0c4683781152f1ada88c3151c57b8cfa27b76
wcotrinac <wilmer_drakerr@hotmail.com>
2016-03-12 20:48:53 -0500

    comit inicial

---
+++ appgit/src/fuentes/Persona.java
@@ -0,0 +1,17 @@
+package fuentes;
+
+public class Persona {
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+}
